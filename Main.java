9c08d169cd40cb61a7d8e2d56369e5ff4ec95af6
Cauã Paes <cauapaes87@gmail.com>
2026-07-09 09:24:05 -0300

    Nova Atividade 9072026

--- Main.java
+++ Main.java
@@ -1,16 +1,44 @@
 // Classe Principal que executa o programa
 public class Main {
+
     public static void main(String[] args) {
-        // 1. Instanciando os objetos
+
+        // ===== Exercício anterior =====
         Carro meuCarro = new Carro();
         Moto minhaMoto = new Moto();
 
-        // 2. Chamando o método aumentarVelocidade para o Carro e para a Moto
         meuCarro.aumentarVelocidade(50);
         minhaMoto.aumentarVelocidade(40);
 
-        // 3. Mostrando as velocidades resultantes na tela
         System.out.println("Velocidade atual do Carro: " + meuCarro.getVelocidade() + " km/h");
-        System.out.println("Velocidade atual do Moto: " + minhaMoto.getVelocidade() + " km/h");
+        System.out.println("Velocidade atual da Moto: " + minhaMoto.getVelocidade() + " km/h");
+
+        // ===== Novo exercício =====
+        System.out.println("\n------------------------------");
+        System.out.println("TESTE DO CARRO ELÉTRICO");
+        System.out.println("------------------------------");
+
+        CarroEletrico carro = new CarroEletrico("Tesla Model 3");
+
+        System.out.println("Modelo: " + carro.getModelo());
+
+        // Acelera até atingir o limite
+        for (int i = 0; i < 7; i++) {
+            carro.acelerar();
+        }
+
+        // Tenta carregar com o carro em movimento
+        carro.carregarBateria();
+
+        // Freia até parar
+        while (carro.getVelocidadeAtual() > 0) {
+            carro.frear();
+        }
+
+        // Carrega corretamente
+        carro.carregarBateria();
+
+        System.out.println("Velocidade Final: " + carro.getVelocidadeAtual() + " km/h");
+        System.out.println("Bateria Final: " + carro.getCargaBateria() + "%");
     }
 }
