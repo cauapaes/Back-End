36fd6ccfec14a997cd5eb57992455b4d867b1720
Cauã Paes <cauapaes87@gmail.com>
2026-07-09 09:32:12 -0300

    Implement factorial calculation in Fatorial.js

---
+++ Fatorial.js
@@ -0,0 +1,9 @@
+let numero = Number(prompt("Digite um número:"));
+
+let fatorial = 1;
+
+for (let i = 1; i <= numero; i++) {
+    fatorial *= i;
+}
+
+console.log("Fatorial de " + numero + " = " + fatorial);
