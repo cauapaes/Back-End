658f2cd9f5e11bda2944234b3b3454f5119e51dc
Cauã Paes <cauapaes87@gmail.com>
2026-07-09 09:32:33 -0300

    Create Positivo ou Negativo.js

---
+++ Positivo ou Negativo.js
@@ -0,0 +1,9 @@
+let numero = Number(prompt("Digite um número:"));
+
+if (numero > 0) {
+    console.log("Número positivo.");
+} else if (numero < 0) {
+    console.log("Número negativo.");
+} else {
+    console.log("O número é zero.");
+}
