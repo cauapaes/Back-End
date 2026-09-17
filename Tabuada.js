3bb5b33cec09648e74c16cc433f8224d76286a66
Cauã Paes <cauapaes87@gmail.com>
2026-09-17 14:40:34 -0300

    Delete Tabuada.js

--- Tabuada.js
+++
@@ -1,6 +0,0 @@
-let numero = Number(prompt("Digite um número:"));
-let limite = Number(prompt("Até qual multiplicador deseja ver a tabuada?"));
-
-for (let i = 1; i <= limite; i++) {
-    console.log(numero + " x " + i + " = " + (numero * i));
-}
