613013e14b57d84ef4de5762ef0a2e3860b3bb26
Cauã Paes <cauapaes87@gmail.com>
2026-09-17 14:41:44 -0300

    Delete Positivo ou Negativo.js

--- Positivo ou Negativo.js
+++
@@ -1,9 +0,0 @@
-let numero = Number(prompt("Digite um número:"));
-
-if (numero > 0) {
-    console.log("Número positivo.");
-} else if (numero < 0) {
-    console.log("Número negativo.");
-} else {
-    console.log("O número é zero.");
-}
